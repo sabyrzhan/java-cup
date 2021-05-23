e982450e3ea0ad10724d753ba9f7d84078bc8000
Sabyrzhan Tynybayev <sabyrzhan1988@gmail.com>
2021-05-23 13:44:41 +0300

    Initial commit

---
+++ src/main/java/kz/sabyrzhan/javacup/JavaCupApplication.java
@@ -0,0 +1,13 @@
+package kz.sabyrzhan.javacup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaCupApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaCupApplication.class, args);
+	}
+
+}
