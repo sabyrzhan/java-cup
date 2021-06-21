97b3791b353b2853ce25c8c59d01cc3c9b3c6c96
Sabyrzhan Tynybayev <sabyrzhan1988@gmail.com>
2021-06-22 02:31:51 +0300

    Add RateLimiter and ExecutorService to FakeSoapEndpoint

--- soap-service/src/main/java/kz/sabyrzhan/soap/endpoints/FakeWsEndpoint.java
+++ soap-service/src/main/java/kz/sabyrzhan/soap/endpoints/FakeWsEndpoint.java
@@ -1,6 +1,9 @@
 package kz.sabyrzhan.soap.endpoints;
 
+import com.google.common.util.concurrent.RateLimiter;
 import org.apache.commons.io.IOUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,6 +13,8 @@ import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.util.Random;
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutorCompletionService;
+import java.util.concurrent.ExecutorService;
 
 import static java.util.concurrent.CompletableFuture.completedFuture;
 
@@ -17,16 +22,25 @@ import static java.util.concurrent.CompletableFuture.completedFuture;
 @RequestMapping("/ws")
 public class FakeWsEndpoint {
     Random random = new Random();
+
+    @Autowired
+    @Qualifier("mainExecutorService")
+    private ExecutorService mainExecutorService;
+
+    @Autowired
+    private RateLimiter rateLimiter;
+
     @PostMapping
     public CompletableFuture<String> getUser(@RequestBody String body) {
-        return completedFuture(null).thenApply(v -> {
+        rateLimiter.acquire();
+        return CompletableFuture.supplyAsync(() -> {
             try {
-                Thread.sleep(random.nextInt(5));
+//                Thread.sleep(random.nextInt(5));
                 String content = IOUtils.toString(getClass().getClassLoader().getResource("response.xml"), StandardCharsets.UTF_8);
                 return content;
             } catch (Exception e) {
                 return "";
             }
-        });
+        }, mainExecutorService);
     }
 }
